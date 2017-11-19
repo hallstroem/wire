e8414a4e41a822fcf50adaa2a27a5cc8c7b38e5e
vagrant <vagrant@node-2>
2017-11-19 12:51:35 +0000

    client determines ip of node-1

--- client.py
+++ client.py
@@ -1,17 +1,18 @@
 import socket
 
 def Main():
-    host = '192.168.192.37'
-    port = 12345
-    mySocket = socket.socket()
-    mySocket.connect((host,port))
-    message = input(" ? ")
-    while message != 'q':
-        mySocket.send(message.encode())
-        data = mySocket.recv(1024).decode()
-        print ('Received from server: ' + data)
-        message = input(" ? ")
-    mySocket.close()
+	hostname='node-1'
+	hostip=socket.gethostbyname(hostname)
+	port = 12345
+	mySocket = socket.socket()
+	mySocket.connect((hostip,port))
+	message = input(" ? ")
+	while message != 'q':
+		mySocket.send(message.encode())
+		data = mySocket.recv(1024).decode()
+		print ('Received from server: ' + data)
+		message = input(" ? ")
+	mySocket.close()
 
 if __name__ == '__main__':
-    Main()
+	Main()
